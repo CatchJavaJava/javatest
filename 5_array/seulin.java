32812a366c5dfb135f8becf7bc44eaba30ca6fdf
seulin <seulin1024@gmail.com>
2022-10-07 17:35:34 +0900

    seulin

--- 5_array/seulin.java
+++ 5_array/seulin.java
@@ -1,18 +1,67 @@
 import java.util.Scanner;
-public class seulin {
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    int arr1[] = new int[10];
-    double avg=0.0;
-    int sum=0;
-    for (int i=0; i<10; i++){
-      arr1[i] = scanner.nextInt();
-      sum += arr1[i];
-    }
-    avg = sum / 10.0;
-    System.out.println(avg);
-    System.out.println(sum);
-    
-    scanner.close();
-}
-}
+// 연습문제 1번 
+//public class seulin {
+//
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		Scanner scanner = new Scanner(System.in);
+//	    int arr1[] = new int[10];
+//	    double avg=0.0;
+//	    int sum=0;
+//	    for (int i=0; i<10; i++){
+//	      arr1[i] = scanner.nextInt();
+//	      sum += arr1[i];
+//	    }
+//	    avg = sum / 10.0;
+//	    System.out.println(avg);
+//	    System.out.println(sum);
+//	    
+//	    scanner.close();
+//	}
+//
+//}
+
+
+// 연습문제 2번
+// 5개 정수를 입력받은 후 차례로 출력하는 프로그램
+//public class seulin{
+//	public static void main(String[] args) {
+//		Scanner scanner = new Scanner(System.in);
+//		int arr1[] = new int[5];
+//		System.out.println("5개의 정수를 입력하세요 : ");
+//		for (int i=0; i<5; i++) {
+//			
+//			arr1[i] = scanner.nextInt();
+//		}
+//		
+//		for (int j=0; j<5; j++) {
+//			System.out.println(arr1[j]);
+//		}
+//		
+//	}
+//}
+
+
+// 연습문제 3번 
+// 10개의 정수를 입력받아 그 중 가장 큰 수를 출력하는 프로그램
+
+//public class seulin{
+//	public static void main(String[] args) {
+//		Scanner scanner = new Scanner(System.in);
+//		int arr1[] = new int [10];
+//		int max = 0;
+//		System.out.println("10개의 정수를 입력하세요 : ");
+//		
+//		for (int i=0; i<10; i++) {
+//			arr1[i] = scanner.nextInt();
+//		}
+//		
+//		for (int j=0; j<9; j++) {
+//			if (arr1[j+1]>arr1[j]) {
+//				max = arr1[j+1];
+//			}
+//		}
+//		System.out.println(max);
+//	}
+//}
+
