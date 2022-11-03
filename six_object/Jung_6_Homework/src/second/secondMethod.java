eed7c4fc8d1ec5b6a67920adc41cf43fe5b562d5
희정 <ggy3r3@naver.com>
2022-11-04 00:32:07 +0900

    Hee_6_Homework

---
+++ six_object/Jung_6_Homework/src/second/secondMethod.java
@@ -0,0 +1,10 @@
+package second;
+
+public class secondMethod {
+
+	double average(int x, int y, int z) {
+		double result = (double)(x + y + z) / 3;
+		return result;
+	}
+	
+}
---
+++ six_object/Jung_6_Homework/src/third/thirdMethod.java
@@ -0,0 +1,12 @@
+package third;
+
+public class thirdMethod {
+
+	int bigcheck(int x, int y) {
+		if(x >= y)
+			return x;
+		else
+			return y;
+	}
+	
+}
